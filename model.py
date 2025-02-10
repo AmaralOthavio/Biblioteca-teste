21aa547f5c4f9a99d46865df8a3e47c6fd01531c
AmaralOthavio <167528849+AmaralOthavio@users.noreply.github.com>
2025-02-10 11:16:09 -0300

    Progresso na API
    Foi adicionado algumas funcionalidades e houveram correções em model.py

--- model.py
+++ model.py
@@ -4,6 +4,14 @@ import fdb
 class Livros:
     def __init__(self, id_livro, titulo, autor, ano_publicacao):
         self.id_livro = id_livro
-        self.id_livro = titulo
-        self.id_livro = autor
-        self.id_livro = ano_publicacao
+        self.titulo = titulo
+        self.autor = autor
+        self.ano_publicacao = ano_publicacao
+
+
+class Usuarios:
+    def __init__(self, id_usuario, nome, senha, email):
+        self.id_usuario = id_usuario
+        self.nome = nome
+        self.senha = senha
+        self.email = email
--- view.py
+++ view.py
@@ -74,3 +74,180 @@ def livro_put(id_livro):
             'ano_publicacao': ano_publicacao
         }
     })
+
+
+@app.route('/livro/<int:id_livro>', methods=['DELETE'])
+def deletar_livro(id_livro):
+    cursor = con.cursor()
+
+    cursor.execute("SELECT 1 FROM livros WHERE ID_LIVRO = ?", (id_livro,))
+    if not cursor.fetchone():
+        cursor.close()
+        return jsonify({"error": "Livro não encontrado"}), 404
+
+    cursor.execute("DELETE FROM livros WHERE ID_LIVRO = ?", (id_livro,))
+    con.commit()
+    cursor.close()
+
+    return jsonify({
+        'message': "Livro excluído com sucesso!",
+        'id_livro': id_livro
+    })
+
+# Rotas de usuário a partir daqui
+
+
+@app.route('/usuarios', methods=['GET'])
+def trazer_usuarios():
+    cur = con.cursor()
+    cur.execute("SELECT ID_USUARIO, NOME, EMAIL, SENHA FROM USUARIOS")
+    usuarios = cur.fetchall()
+    usuarios_dic = []
+    for usuario in usuarios:
+        usuarios_dic.append({
+            'id_usuario': usuario[0],
+            'nome': usuario[1],
+            'email': usuario[2],
+            'senha': usuario[3]
+        })
+    return jsonify(usuarios=usuarios_dic, mensagem='Lista de usuários')
+
+
+@app.route('/usuario', methods=['POST'])
+def inserir_usuario():
+    data = request.get_json()
+    nome = data.get('nome')
+    email = data.get('email')
+    senha = data.get('senha')
+
+    # Validações de senha
+    simbolos = ['!', '@', '#', '$', '%', '¨', '&', '*', '(', ')', '-', '_', '+', '=', '§', '"', "'",
+                '|', ':', ';', '?', '°', '<', '>', '{', '}', '[', ']', ',', '.', '*', '~', '´', '`',
+                'º', 'ª', '/', '^', '¹', '²', '³', '£', '¢', '¬']
+    numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    numeros_usados = 0
+    simbolos_usados = 0
+    for simbolo in simbolos:
+        if simbolo in senha:
+            simbolos_usados += 1
+    for numero in numeros:
+        if numero in senha:
+            numeros_usados += 1
+
+    if len(senha) < 8:
+        return jsonify(mensagem='Erro: A senha precisa ter pelo menos 8 caracteres')
+    if simbolos_usados < 1:
+        return jsonify(mensagem='Erro: A senha precisa ter pelo menos 1 símbolo do seu teclado')
+    if numeros_usados < 1:
+        return jsonify(mensagem='Erro: A senha precisa ter pelo menos um número')
+    # Fim das validações de senha
+
+    cur = con.cursor()
+    cur.execute("SELECT 1 FROM USUARIOS WHERE email = ?", (email,))
+
+    if cur.fetchone():
+        return jsonify(mensagem='Este e-mail já possui uma conta')
+
+    cur.execute("INSERT INTO USUARIOS(NOME, EMAIL, SENHA) VALUES (?, ?, ?)", (nome, email, senha))
+
+    con.commit()
+    cur.close()
+
+    return jsonify({
+        'mensagem': 'Usuário cadastrado com sucesso',
+        'usuario': {
+            'nome': nome,
+            'email': email,
+            'senha': senha
+        }
+    })
+
+
+@app.route('/usuario/<int:id_usuario>', methods=['PUT'])
+def editar_usuario(id_usuario):
+    cur = con.cursor()
+    cur.execute("SELECT ID_USUARIO, NOME, EMAIL, SENHA FROM USUARIOS WHERE ID_USUARIO = ?", (id_usuario, ))
+    usuario_data = cur.fetchone()
+
+    if not usuario_data:
+        cur.close()
+        return jsonify(mensagem="Usuário não foi encontrado")
+
+    data = request.get_json()
+    nome = data.get('nome')
+    email = data.get('email')
+    senha = data.get('senha')
+
+    # Validações de senha
+    simbolos = ['!', '@', '#', '$', '%', '¨', '&', '*', '(', ')', '-', '_', '+', '=', '§', '"', "'",
+                '|', ':', ';', '?', '°', '<', '>', '{', '}', '[', ']', ',', '.', '*', '~', '´', '`',
+                'º', 'ª', '/', '^', '¹', '²', '³', '£', '¢', '¬']
+    numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    numeros_usados = 0
+    simbolos_usados = 0
+    for simbolo in simbolos:
+        if simbolo in senha:
+            simbolos_usados += 1
+    for numero in numeros:
+        if numero in senha:
+            numeros_usados += 1
+
+    if len(senha) < 8:
+        cur.close()
+        return jsonify(mensagem='Erro: A senha precisa ter pelo menos 8 caracteres')
+    if simbolos_usados < 1:
+        cur.close()
+        return jsonify(mensagem='Erro: A senha precisa ter pelo menos 1 símbolo do seu teclado')
+    if numeros_usados < 1:
+        cur.close()
+        return jsonify(mensagem='Erro: A senha precisa ter pelo menos um número')
+    # Fim das validações de senha
+
+    cur.execute("UPDATE USUARIOS SET NOME = ?, EMAIL = ?, SENHA = ? WHERE ID_USUARIO = ?",
+                (nome, email, senha, id_usuario))
+    con.commit()
+    con.close()
+
+    return jsonify({
+        'message': 'Usuário editado com sucesso!',
+        'livro': {
+            'nome': nome,
+            'email': email,
+            'senha': senha
+        }
+    })
+
+
+@app.route('/usuario/<int:id_usuario>', methods=['DELETE'])
+def deletar_usuario(id_usuario):
+    cursor = con.cursor()
+
+    cursor.execute("SELECT 1 FROM USUARIOS WHERE ID_USUARIO = ?", (id_usuario,))
+    if not cursor.fetchone():
+        cursor.close()
+        return jsonify({"error": "Usuário não encontrado"}), 404
+
+    cursor.execute("DELETE FROM USUARIOS WHERE ID_USUARIO = ?", (id_usuario,))
+    con.commit()
+    cursor.close()
+
+    return jsonify({
+        'message': "Usuário excluído com sucesso!",
+        'id_livro': id_usuario
+    })
+
+
+@app.route('/login', methods=['POST'])
+def efetuar_login():
+    data = request.get_json()
+    email = data.get('email')
+    senha = data.get('senha')
+
+    cur = con.cursor()
+    cur.execute('SELECT ID_USUARIO FROM USUARIOS WHERE EMAIL = ? AND SENHA = ?', (email, senha,))
+    usuario = cur.fetchone()
+
+    if usuario:
+        return jsonify(mensagem='Login efetuado com sucesso')
+
+    return jsonify(mensagem="Login falhado, usuário ou senha incorretos")
